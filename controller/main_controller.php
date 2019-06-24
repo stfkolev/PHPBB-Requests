a2c3630d61edb2ab2192f30a799a625e468b388d
Stf Kolev <inkyzfx@gmail.com>
2019-06-24 15:44:22 +0300

    Added BBCodes for the reply page

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -254,6 +254,8 @@ class main_controller
 				generate_smilies('inline', 0);
 
 				$this->template->assign_vars([
+					'S_LINKS_ALLOWED'	=> true,
+					'S_BBCODE_ALLOWED'	=> true,
 					'S_BBCODE_IMG'		=> true,
 					'S_BBCODE_QUOTE'	=> true,
 					'S_BBCODE_FLASH'	=> true,
@@ -292,11 +294,14 @@ class main_controller
 					/*! Find Author of reply */
 					$sql = 'SELECT * FROM ' . USERS_TABLE . ' WHERE ' . $this->db->sql_build_array('SELECT', $find);
 					$author = $this->db->sql_fetchrow($this->db->sql_query($sql));
+					$authorAvatar = get_user_avatar($author['user_avatar'], $author['user_avatar_type'], $author['user_avatar_width'], $author['user_avatar_height']);
+
 
 					/*! Assign block of variables */
 					$this->template->assign_block_vars('reply', array(
 						'REPLY_AUTHOR'					=> $author['username'],
 						'REPLY_AUTHOR_COLOUR'			=> $author['user_colour'],
+						'REPLY_AUTHOR_AVATAR'			=> $authorAvatar,
 						'REPLY_ADDITIONAL'				=> $row['replies_additional']
 					));
 				}
@@ -308,14 +313,19 @@ class main_controller
 
 				$sql = 'SELECT * FROM ' . USERS_TABLE . ' WHERE ' . $this->db->sql_build_array('SELECT', $data);
 				$author = $this->db->sql_fetchrow($this->db->sql_query($sql));
-
-				var_dump($request);
+				
+				$authorAvatar = get_user_avatar($author['user_avatar'], $author['user_avatar_type'], $author['user_avatar_width'], $author['user_avatar_height']);
+				$authorRank = phpbb_get_user_rank($author, $author['user_posts']);
+				
 
 				/*! Assing basic variables */
 				$this->template->assign_vars(array(
 					'REQUEST_ID'				=> $request['requests_id'],
 					'REQUEST_AUTHOR'			=> $author['username'],
 					'REQUEST_AUTHOR_COLOR'		=> $author['user_colour'],
+					'REQUEST_AUTHOR_AVATAR'		=> $authorAvatar,
+					'REQUEST_AUTHOR_RANK'		=> $authorRank['img'] ?: $authorRank['title'],
+					'REQUEST_AUTHOR_POSTS'		=> $author['user_posts'],
 					'REQUEST_TITLE'				=> $request['requests_title'],
 					'REQUEST_TYPE'				=> $request['requests_type'],
 					'REQUEST_ADDITIONAL'		=> $request['requests_additional'],
@@ -327,13 +337,18 @@ class main_controller
 				/*! If is user registered */	
 				if($this->user->data['is_registered']) {
 					/*! Add CSRF */
-					add_form_key('replies_add');
+					add_form_key('postform');
 
 					$errors = array();
 					
 					/*! Check if request is post */
 					if($this->request->is_set_post('submit')) {
-						// Add replies record
+						var_dump($this->request->variable('message', ''));
+						
+						/*! Redirect after 3 seconds if no action is taken */
+						meta_refresh(3, $this->helper->route('evilsystem_requests_controller', array('name' => 'all')));
+						$message = $this->language->lang('REQUESTS_REQUEST_ADDED') . '<br /><br />' . $this->language->lang('REQUESTS_RETURN', '<a href="' . $this->helper->route('evilsystem_requests_controller', array('name' => 'all')) . '">', '</a>');
+						trigger_error($message);
 					}
 
 				} else 
